a0d834c439193f79b67deae78cda0562804ed123
Lean Beef Patty <123946674+Thenukee@users.noreply.github.com>
2024-04-22 02:38:12 +0530

    substitute Enc
    simple substitute encryption program

---
+++ Java/Encrypter/Main.java
@@ -0,0 +1,11 @@
+package Java.Encrypter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		EncryptionProgram ep = new EncryptionProgram();
+
+	}
+
+}
--- Java/SerializerAndDeserializer/Deserializer.java
+++ Java/SerializerAndDeserializer/Deserializer.java
@@ -36,6 +36,12 @@ public class Deserializer {
 
     }
 
+    //                 	1. children classes of a parent class that implements Serializable will do so as well
+	   //!					2. static fields are not serialized (they belong to the class, not an individual object)
+	   //!					3. Fields declared as "transient" aren't serialized, they're ignored
+	   //					4. the class's definition ("class file") itself is not recorded, cast it as the object type
+	   //!					5. serialVersionUID is a unique version ID 
+
 
 
 
